8ad6762faf2af88b49f17182d6986c39bc5835e7
bwlodarczykjj <b.wlodarczykjj@gmail.com>
2019-06-08 20:02:46 +0200

    Install styled-components, defined themes, standardized fonts, set happy rems and added import absoluted plugin

---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css?family=Montserrat:300,800&display=swap');
+
+    *, *::before, *::after {
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        box-sizing: border-box;
+        
+    }
+
+    html {
+        font-size: 62.5%;
+    }
+
+    body {
+        font-size: 1.6rem;
+        font-family: "Montserrat", sans-serif;
+    }
+`;
+
+export default GlobalStyle;
