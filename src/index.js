4d42745af182c42d2548da346b2cf25036be7c91
bwlodarczykjj <b.wlodarczykjj@gmail.com>
2019-06-23 14:57:56 +0200

    Added exception in a DetailsPage.js file to disable checking a props. I also added exception to .env file to protect my app from crash (different version of babel-eslint). I wrote and added a handleClick function in a DetailsPage file to handle transition into details view of notes, twitters and articles.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from 'views/Root/Root';
 
-/* eslint-disable no-undef    */
+// eslint-disable-next-line no-undef
 ReactDOM.render(<Root />, document.getElementById('root'));
-/* eslint-disable no-undef  */
